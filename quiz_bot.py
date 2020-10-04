09addefb07b36402c6b820c5423d3774a2de7599
Klemets Kristian <kristian.klemets@gmail.com>
2020-10-04 15:04:00 +0300

    Update script and add answers

--- quiz_bot.py
+++ quiz_bot.py
@@ -15,14 +15,7 @@ ANSWER_FILE = "answers.pkl"
 
 
 def get_driver():
-    mobile_emulation = {
-        "deviceMetrics": {"width": 360, "height": 640, "pixelRatio": 3.0},
-        "userAgent": "Mozilla/5.0 (Linux; Android 4.2.1; en-us; Nexus 5 Build/JOP40D) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.166 Mobile Safari/535.19",
-    }
-    chrome_options = Options()
-    chrome_options.add_experimental_option("mobileEmulation", mobile_emulation)
-    driver = webdriver.Chrome(chrome_options=chrome_options)
-
+    driver = webdriver.Chrome()
     driver.get("http://benz.me/hextech-chest-quiz")
     driver.switch_to.frame("frame")
     return driver
