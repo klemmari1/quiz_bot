a12a49c7cf6d4a2127f541f0c57b0e60725a2496
Klemets Kristian <kristian.klemets@gmail.com>
2020-10-04 15:44:41 +0300

    Update answers and showing the answers

--- show_answers.py
+++ show_answers.py
@@ -1,4 +1,5 @@
 import pickle
+import pprint
 from os import path
 
 ANSWER_FILE = "answers.pkl"
@@ -6,6 +7,4 @@ ANSWER_FILE = "answers.pkl"
 
 if path.isfile(ANSWER_FILE):
     with open(ANSWER_FILE, "rb") as answer_file:
-        print(pickle.load(answer_file))
-else:
-    print()
+        pprint.pprint(pickle.load(answer_file))
