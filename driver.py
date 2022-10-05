8ea44526b8b1b757609c972bd4329812d2281292
Kristian Klemets <kristian.klemets@unity3d.com>
2022-10-05 11:54:41 +0300

    Add stealth back

--- driver.py
+++ driver.py
@@ -1,7 +1,23 @@
 import undetected_chromedriver.v2 as uc
+from selenium_stealth import stealth
 
 
 def get_driver():
-    driver = uc.Chrome(version_main=105)
+    options = uc.ChromeOptions()
+
+    options.add_argument("--window-size=480,860")
+    options.add_argument("--no-first-run --no-service-autorun --password-store=basic")
+
+    driver = uc.Chrome(version_main=105, options=options)
+
+    stealth(
+        driver,
+        languages=["en-US", "en"],
+        vendor="Google Inc.",
+        platform="Win32",
+        webgl_vendor="Intel Inc.",
+        renderer="Intel Iris OpenGL Engine",
+        fix_hairline=True,
+    )
 
     return driver
