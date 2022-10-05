bf9a671468c7ff86088fa3bc067dcf88df981a17
Kristian Klemets <kristian.klemets@unity3d.com>
2022-10-05 10:08:44 +0300

    Use click actions

--- button.py
+++ button.py
@@ -1,10 +1,6 @@
 import random
 
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.actions import interaction
-from selenium.webdriver.common.actions.action_builder import ActionBuilder
-from selenium.webdriver.common.actions.pointer_input import PointerInput
 from selenium.webdriver.remote.webelement import WebElement
 
 
@@ -12,27 +8,36 @@ def button_click(driver: webdriver.Chrome, button: WebElement, pause=True):
     xrand = button.size["width"] * random.uniform(0.4, 0.6)
     yrand = button.size["height"] * random.uniform(0.4, 0.6)
 
-    # action = webdriver.ActionChains(driver)
+    action = webdriver.ActionChains(driver)
 
-    # action.pause(random.uniform(0.8, 1.3)).move_to_element_with_offset(
-    #     button, xrand, yrand
-    # ).pause(random.uniform(0.2, 0.5)).click_and_hold().pause(
-    #     random.uniform(0.1, 0.2)
-    # ).release()
-    # action.perform()
+    action.pause(
+            random.uniform(0.8, 1.5)
+        ).move_to_element_with_offset(
+            button, xrand, yrand
+        ).pause(
+            random.uniform(0.2, 0.5)
+        ).click_and_hold().pause(
+            random.uniform(0.1, 0.2)
+        ).release()
+    action.perform()
 
-    actions = ActionChains(driver)
-    actions.w3c_actions = ActionBuilder(
-        driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch")
-    )
+    # from selenium.webdriver.common.action_chains import ActionChains
+    # from selenium.webdriver.common.actions import interaction
+    # from selenium.webdriver.common.actions.action_builder import ActionBuilder
+    # from selenium.webdriver.common.actions.pointer_input import PointerInput
 
-    if pause:
-        actions.w3c_actions.pointer_action.pause(random.uniform(0.5, 1.0))
-    actions.w3c_actions.pointer_action.move_to(button, xrand, yrand)
-    if pause:
-        actions.w3c_actions.pointer_action.pause(random.uniform(0.2, 0.5))
-    actions.w3c_actions.pointer_action.click_and_hold()
-    if pause:
-        actions.w3c_actions.pointer_action.pause(random.uniform(0.1, 0.2))
-    actions.w3c_actions.pointer_action.release()
-    actions.w3c_actions.perform()
+    # actions = ActionChains(driver)
+    # actions.w3c_actions = ActionBuilder(
+    #     driver, mouse=PointerInput(interaction.POINTER_TOUCH, "touch")
+    # )
+
+    # if pause:
+    #     actions.w3c_actions.pointer_action.pause(random.uniform(0.5, 1.0))
+    # actions.w3c_actions.pointer_action.move_to(button, xrand, yrand)
+    # if pause:
+    #     actions.w3c_actions.pointer_action.pause(random.uniform(0.2, 0.5))
+    # actions.w3c_actions.pointer_action.click_and_hold()
+    # if pause:
+    #     actions.w3c_actions.pointer_action.pause(random.uniform(0.1, 0.2))
+    # actions.w3c_actions.pointer_action.release()
+    # actions.w3c_actions.perform()
